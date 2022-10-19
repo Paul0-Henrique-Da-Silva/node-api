fe047537be1eebdbb4073a05e0f19c872ead9f89
Paulo Henrique <ph46163835@gmail.com>
2022-10-19 18:34:19 -0300

    test: ensure SignController returns 200 if valid date is provided ---commit before is a feat

--- __tests__/signup.test.ts
+++ __tests__/signup.test.ts
@@ -199,4 +199,24 @@ describe('SinUp Controllers', () => {
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
+
+  test('Email valido, "return code 200"', () => {
+    const { sut } = makesut()
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_email@gmail.com',
+        password: 'valid_password',
+        passwordConfirm: 'valid_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@gmail.com',
+      password: 'valid_password'
+    })
+  })
 })
