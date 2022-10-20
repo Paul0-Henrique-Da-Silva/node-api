dd8e3a58bb98f4f7640664d3879aae7dad5ddf61
Paulo Henrique <ph46163835@gmail.com>
2022-10-20 00:21:14 -0300

    refactor: change file name

--- __tests__/email-validator-adapter.test.ts
+++ __tests__/email-validator-adapter.test.ts
@@ -1,20 +1,24 @@
 import { EmailValidatorAdapter } from '../src/utils/email-validator'
 import validator from 'validator'
 
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
 describe('EmailValidator', () => {
   test('Se retorno for falso , retorne "false"', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@gmail.com')
     expect(isValid).toBe(false)
   })
   test('Se retorno for verdadeiro , retorne "true"', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid('valid_email@gmail.com')
     expect(isValid).toBe(true)
   })
   test(' Chame email correto com validator ', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
     sut.isValid('any_email@gmail.com')
     expect(isEmailSpy).toHaveBeenCalledWith('any_email@gmail.com')
