140611f7b8b33a0d7901ff6e4cd080a1fabffb64
Paulo <ph46163835@gmail.com>
2023-01-11 17:26:12 -0300

    test: ensure requests return json as default content type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,10 @@
 import { Express } from 'express'
 import { boryParser } from '../midlewares/body-parser'
+import { contentType } from '../midlewares/content-type'
+import { cors } from '../midlewares/cors'
 
 export default (app: Express): void  => {
     app.use(boryParser)
+    app.use(cors)
+    app.use(contentType)
 }
---
+++ src/main/midlewares/content-type.ts
@@ -0,0 +1,6 @@
+import { Request, Response, NextFunction } from 'express'
+
+export const contentType = (request: Request, response: Response, next: NextFunction) => {
+    response.type('json')
+    next()
+}
